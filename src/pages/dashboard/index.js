d3c4453debd0fd98bee3653e90d9ab30ed481fca
Jade <gjcaldabasab1010@gmail.com>
2024-01-07 22:25:13 +0800

    new update

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,89 +1,28 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-
 //material-ui
-import {
-  Avatar,
-  AvatarGroup,
-  Box,
-  Button,
-  Grid,
-  List,
-  ListItemAvatar,
-  ListItemButton,
-  ListItemSecondaryAction,
-  ListItemText,
-  MenuItem,
-  Stack,
-  TextField,
-  Typography
-} from '@mui/material';
+import { Box, Button, Grid, Stack, Typography } from '@mui/material';
 
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import {Icon} from 'leaflet';
-import "leaflet/dist/leaflet.css";
-import "./style.css";
+import { Icon } from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import './style.css';
 
 // project import
-import OrdersTable from './OrdersTable';
 import IncomeAreaChart from './IncomeAreaChart';
 import MonthlyBarChart from './MonthlyBarChart';
-import ReportAreaChart from './ReportAreaChart';
-import SalesColumnChart from './SalesColumnChart';
 import MainCard from 'components/MainCard';
 import AnalyticEcommerce from 'components/cards/statistics/AnalyticEcommerce';
-
-import {
-  GeoapifyGeocoderAutocomplete,
-  GeoapifyContext,
-} from "@geoapify/react-geocoder-autocomplete";
-
-//assets
-import { GiftOutlined, MessageOutlined, SettingOutlined } from '@ant-design/icons';
-import avatar1 from 'assets/images/users/avatar-1.png';
-import avatar2 from 'assets/images/users/avatar-2.png';
-import avatar3 from 'assets/images/users/avatar-3.png';
-import avatar4 from 'assets/images/users/avatar-4.png';
-
-//avatar style
-const avatarSX = {
-  width: 36,
-  height: 36,
-  fontSize: '1rem'
-};
-
-//action style
-const actionSX = {
-  mt: 0.75,
-  ml: 1,
-  top: 'auto',
-  right: 'auto',
-  alignSelf: 'flex-start',
-  transform: 'none'
-};
-
-//sales report status
-const status = [
-  {
-    value: 'today',
-    label: 'Today'
-  },
-  {
-    value: 'month',
-    label: 'This Month'
-  },
-  {
-    value: 'year',
-    label: 'This Year'
-  }
-];
+import { FormControl, InputLabel, MenuItem, Select } from '../../../node_modules/@mui/material/index';
 
 // ==============================|| DASHBOARD - DEFAULT ||============================== //
 
 const DashboardDefault = () => {
   const [value, setValue] = useState('today');
+  const [filterLoc, setFilterLoc] = useState(null);
   const [slot, setSlot] = useState('month');
   const [data, setData] = useState({});
+  const [planTypes, setPlanTypes] = useState([]);
   const [consumption, setConsumption] = useState([]);
   const [income, setIncome] = useState([]);
   const [location, setLocation] = useState([]);
@@ -101,6 +40,7 @@ const DashboardDefault = () => {
           setIncome(res.data?.income);
           setData(res.data);
           setLocation(res.data.location);
+          populatePlanTypes();
         }
       })
       .catch((error) => {
@@ -108,26 +48,68 @@ const DashboardDefault = () => {
       });
   }, []);
 
+  const populatePlanTypes = async () => {
+    const config = {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+    };
+    axios
+      .get(`${process.env.REACT_APP_BASE_API_URL}/get-plan-type`, config)
+      .then((res) => {
+        if (res) {
+          console.log('plan type', res.data);
+          setPlanTypes(res.data);
+        } else {
+          console.log(res.data.message);
+        }
+      })
+      .catch((error) => {
+        console.log('err', error);  
+      });
+  };
+
+  const filterSubLocation = (e) => {
+    const target = e.target;
+    // setFilterLoc(target.value);
+    // console.log('target value', target.value)
+    const config = {
+      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+    };
+    axios
+      .post(`${process.env.REACT_APP_BASE_API_URL}/filter-subsloc`, {plan_id: target.value} , config)
+      .then((res) => {
+        if (res) {
+          console.log('result', res);
+          setLocation(res.data)
+          setFilterLoc(target.value);
+        } else {
+          console.log(res.data.message);
+        }
+      })
+      .catch((error) => {
+        console.log('err', error);
+      });
+  };
+
   const customIcon1 = new Icon({
-    iconUrl : require('../../assets/images/icons/location-pin-orange.png'),
-    iconSize: [35,35]
-  })
+    iconUrl: require('../../assets/images/icons/location-pin-orange.png'),
+    iconSize: [35, 35]
+  });
   const customIcon2 = new Icon({
-    iconUrl : require('../../assets/images/icons/location-pin-blue.png'),
-    iconSize: [35,35]
-  })
+    iconUrl: require('../../assets/images/icons/location-pin-blue.png'),
+    iconSize: [35, 35]
+  });
   const customIcon3 = new Icon({
-    iconUrl : require('../../assets/images/icons/location-pin-red.png'),
-    iconSize: [35,35]
-  })
+    iconUrl: require('../../assets/images/icons/location-pin-red.png'),
+    iconSize: [35, 35]
+  });
   const customIcon4 = new Icon({
-    iconUrl : require('../../assets/images/icons/location-pin-purple.png'),
-    iconSize: [35,35]
-  })
+    iconUrl: require('../../assets/images/icons/location-pin-purple.png'),
+    iconSize: [35, 35]
+  });
   const customIcon5 = new Icon({
-    iconUrl : require('../../assets/images/icons/location-pin-lightblue.png'),
-    iconSize: [35,35]
-  })
+    iconUrl: require('../../assets/images/icons/location-pin-lightblue.png'),
+    iconSize: [35, 35]
+  });
 
   const position = [11.158384, 124.991888];
 
@@ -149,9 +131,6 @@ const DashboardDefault = () => {
       <Grid item xs={12} sm={6} md={4} lg={3}>
         <AnalyticEcommerce title="Subscriber" count={data?.subscriber} />
       </Grid>
-      {/* <Grid item xs={12} sm={6} md={4} lg={3}>
-        <AnalyticEcommerce title="Plan Available" count="0"/>
-      </Grid> */}
       <Grid item md={8} sx={{ display: { sm: 'none', md: 'block', lg: 'none' } }} />
 
       {/* row 2 */}
@@ -170,14 +149,6 @@ const DashboardDefault = () => {
               >
                 Month
               </Button>
-              {/* <Button
-                size="small"
-                onClick={() => setSlot('week')}
-                color={slot === 'week' ? 'primary' : 'secondary'}
-                variant={slot === 'week' ? 'outlined' : 'text'}
-              >
-                Week
-              </Button> */}
             </Stack>
           </Grid>
         </Grid>
@@ -210,8 +181,24 @@ const DashboardDefault = () => {
       {/* row 3 */}
       <Grid item xs={12} md={7} lg={12}>
         <Grid container alignItems="center" justifyContent="space-between">
-          <Grid item>
+          <Grid item xs={9}>
             <Typography variant="h5">Subscriber Location</Typography>
+          </Grid>
+          <Grid item xs={3}>
+            <Stack spacing={1}>
+              <FormControl fullWidth>
+                <InputLabel htmlFor="plan_type_id">Filter Plan Type</InputLabel>
+                <Select label="Type of Plan" id="plan_type_id" name="plan_type_id" value={filterLoc} onChange={filterSubLocation}>
+                  {planTypes
+                    ? planTypes.map((planType) => (
+                        <MenuItem disabled={planType.status == 1 ? false : true} key={planType.id} value={planType.id}>
+                          {planType.mbps} mbps plan
+                        </MenuItem>
+                      ))
+                    : null}
+                </Select>
+              </FormControl>
+            </Stack>
           </Grid>
           <Grid item />
         </Grid>
@@ -221,206 +208,37 @@ const DashboardDefault = () => {
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            {location.length? location.map((loc)=> {
-                return (
-               
-                  <Marker position={[loc.lat, loc.lon]} icon={loc.user.subscriber.plan_type.id == 15 ? customIcon1 : loc.user.subscriber.plan_type.id == 16 ? customIcon2 : loc.user.subscriber.plan_type.id == 17 ? customIcon3 : loc.user.subscriber.plan_type.id == 18 ? customIcon4 : customIcon5 }>
-                    <Popup>
-                      <Typography style={{textTransform: 'capitalize'}}>{loc.user?.first_name} {loc.user?.last_name}</Typography>  {loc.user?.subscriber.plan_type.mbps} mbps
-                    </Popup>
-                  </Marker>
-                )
-           
-             
-             
-            })
-            :
-            null  
-          }
-            
+            { location.length
+              ? location.map((loc) => {
+                  return (
+                    <Marker
+                      position={[loc.lat, loc.lon]}
+                      icon={
+                        loc.user.subscriber.plan_type.id == 15
+                          ? customIcon1
+                          : loc.user.subscriber.plan_type.id == 16
+                          ? customIcon2
+                          : loc.user.subscriber.plan_type.id == 17
+                          ? customIcon3
+                          : loc.user.subscriber.plan_type.id == 18
+                          ? customIcon4
+                          : customIcon5
+                      }
+                    >
+                      <Popup>
+                        <Typography style={{ textTransform: 'capitalize' }}>
+                          {loc.user?.first_name} {loc.user?.last_name}
+                        </Typography>{' '}
+                        {loc.user?.subscriber.plan_type.mbps} mbps
+                      </Popup>
+                    </Marker>
+                  );
+                })
+              : null }
           </MapContainer>
         </MainCard>
       </Grid>
-      {/* <Grid item xs={12} md={5} lg={4}>
-        <Grid container alignItems="center" justifyContent="space-between">
-          <Grid item>
-            <Typography variant="h5">Analytics Report</Typography>
-          </Grid>
-          <Grid item />
-        </Grid>
-        <MainCard sx={{ mt: 2 }} content={false}>
-          <List sx={{ p: 0, '& .MuiListItemButton-root': { py: 2 } }}>
-            <ListItemButton divider>
-              <ListItemText primary="Company Finance Growth" />
-              <Typography variant="h5">+45.14%</Typography>
-            </ListItemButton>
-            <ListItemButton divider>
-              <ListItemText primary="Company Expenses Ratio" />
-              <Typography variant="h5">0.58%</Typography>
-            </ListItemButton>
-            <ListItemButton>
-              <ListItemText primary="Business Risk Cases" />
-              <Typography variant="h5">Low</Typography>
-            </ListItemButton>
-          </List>
-          <ReportAreaChart />
-        </MainCard>
-      </Grid> */}
-
-      {/* row 4 */}
-      {/* <Grid item xs={12} md={7} lg={8}>
-        <Grid container alignItems="center" justifyContent="space-between">
-          <Grid item>
-            <Typography variant="h5">Sales Report</Typography>
-          </Grid>
-          <Grid item>
-            <TextField
-              id="standard-select-currency"
-              size="small"
-              select
-              value={value}
-              onChange={(e) => setValue(e.target.value)}
-              sx={{ '& .MuiInputBase-input': { py: 0.5, fontSize: '0.875rem' } }}
-            >
-              {status.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </TextField>
-          </Grid>
-        </Grid>
-        <MainCard sx={{ mt: 1.75 }}>
-          <Stack spacing={1.5} sx={{ mb: -12 }}>
-            <Typography variant="h6" color="secondary">
-              Net Profit
-            </Typography>
-            <Typography variant="h4">$1560</Typography>
-          </Stack>
-          <SalesColumnChart />
-        </MainCard>
-      </Grid>
-      <Grid item xs={12} md={5} lg={4}>
-        <Grid container alignItems="center" justifyContent="space-between">
-          <Grid item>
-            <Typography variant="h5">Transaction History</Typography>
-          </Grid>
-          <Grid item />
-        </Grid>
-        <MainCard sx={{ mt: 2 }} content={false}>
-          <List
-            component="nav"
-            sx={{
-              px: 0,
-              py: 0,
-              '& .MuiListItemButton-root': {
-                py: 1.5,
-                '& .MuiAvatar-root': avatarSX,
-                '& .MuiListItemSecondaryAction-root': { ...actionSX, position: 'relative' }
-              }
-            }}
-          >
-            <ListItemButton divider>
-              <ListItemAvatar>
-                <Avatar
-                  sx={{
-                    color: 'success.main',
-                    bgcolor: 'success.lighter'
-                  }}
-                >
-                  <GiftOutlined />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary={<Typography variant="subtitle1">Order #002434</Typography>} secondary="Today, 2:00 AM" />
-              <ListItemSecondaryAction>
-                <Stack alignItems="flex-end">
-                  <Typography variant="subtitle1" noWrap>
-                    + $1,430
-                  </Typography>
-                  <Typography variant="h6" color="secondary" noWrap>
-                    78%
-                  </Typography>
-                </Stack>
-              </ListItemSecondaryAction>
-            </ListItemButton>
-            <ListItemButton divider>
-              <ListItemAvatar>
-                <Avatar
-                  sx={{
-                    color: 'primary.main',
-                    bgcolor: 'primary.lighter'
-                  }}
-                >
-                  <MessageOutlined />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary={<Typography variant="subtitle1">Order #984947</Typography>} secondary="5 August, 1:45 PM" />
-              <ListItemSecondaryAction>
-                <Stack alignItems="flex-end">
-                  <Typography variant="subtitle1" noWrap>
-                    + $302
-                  </Typography>
-                  <Typography variant="h6" color="secondary" noWrap>
-                    8%
-                  </Typography>
-                </Stack>
-              </ListItemSecondaryAction>
-            </ListItemButton>
-            <ListItemButton>
-              <ListItemAvatar>
-                <Avatar
-                  sx={{
-                    color: 'error.main',
-                    bgcolor: 'error.lighter'
-                  }}
-                >
-                  <SettingOutlined />
-                </Avatar>
-              </ListItemAvatar>
-              <ListItemText primary={<Typography variant="subtitle1">Order #988784</Typography>} secondary="7 hours ago" />
-              <ListItemSecondaryAction>
-                <Stack alignItems="flex-end">
-                  <Typography variant="subtitle1" noWrap>
-                    + $682
-                  </Typography>
-                  <Typography variant="h6" color="secondary" noWrap>
-                    16%
-                  </Typography>
-                </Stack>
-              </ListItemSecondaryAction>
-            </ListItemButton>
-          </List>
-        </MainCard>
-        <MainCard sx={{ mt: 2 }}>
-          <Stack spacing={3}>
-            <Grid container justifyContent="space-between" alignItems="center">
-              <Grid item>
-                <Stack>
-                  <Typography variant="h5" noWrap>
-                    Help & Support Chat
-                  </Typography>
-                  <Typography variant="caption" color="secondary" noWrap>
-                    Typical replay within 5 min
-                  </Typography>
-                </Stack>
-              </Grid>
-              <Grid item>
-                <AvatarGroup sx={{ '& .MuiAvatar-root': { width: 32, height: 32 } }}>
-                  <Avatar alt="Remy Sharp" src={avatar1} />
-                  <Avatar alt="Travis Howard" src={avatar2} />
-                  <Avatar alt="Cindy Baker" src={avatar3} />
-                  <Avatar alt="Agnes Walker" src={avatar4} />
-                </AvatarGroup>
-              </Grid>
-            </Grid>
-            <Button size="small" variant="contained" sx={{ textTransform: 'capitalize' }}>
-              Need Help?
-            </Button>
-          </Stack>
-        </MainCard>
-      </Grid> */}
     </Grid>
   );
 };
-
 export default DashboardDefault;
